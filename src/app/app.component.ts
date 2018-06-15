efe07a9eb554f3e149b2271ce59e8668c1ee2e30
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-06-15 22:59:05 +0200

    Install & config ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'av-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'av';
+  constructor(translateSrv: TranslateService) {
+    translateSrv.addLangs(['es']);
+    translateSrv.setDefaultLang('es');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,29 @@ import { AppComponent } from './app.component';
 // Modules
 import { VidaModule } from './modules/vida/vida.module';
 
+// Translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     VidaModule,
   ],
   providers: [],
